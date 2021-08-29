0b58d2fc1d5f0cad1f08bc0f387acdb497284826
simran-ux <simranjeet2772000@gmail.com>
2021-08-29 16:09:20 +0530

    Arithmetic Operators in JS

---
+++ ArithmeticOperators.js
@@ -0,0 +1,34 @@
+//Operators are special symbols used to perform operations on numeric operands and produces a result
+//Arithmetic Operators are used to perform mathematical operations between numeric operands.
+
+//variables or operands
+
+var x , y, z;
+
+x = 15;
+y = 10;
+
+//Addition operator 
+
+z = x + y;
+
+document.write("The addition of 10+15 = "+z)
+
+//Subtraction operator
+
+z = x - y;
+
+document.write("The subtraction of 15 - 10 = "+z);
+
+//Multiplication Operator 
+
+z = x * y;
+
+document.write("The multiplication of 15*10 = "+z);
+
+//Modulus Operator
+
+z = x % y;
+
+document.write("The modulus of 15 % 10 is = "+z);
+
