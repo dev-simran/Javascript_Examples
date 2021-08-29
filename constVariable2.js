f8279c943b93f903a284a1a2f493690a50e3093a
simran-ux <simranjeet2772000@gmail.com>
2021-08-29 13:01:25 +0530

    Const variable Assignment and intiliazation

---
+++ constVariable2.js
@@ -0,0 +1,21 @@
+// Declare variable with const example.
+// Const variables must be assigned a value when they are declared.
+
+const pi;
+
+console.log(pi); //give error like missing intialization
+
+
+// correct method
+
+const a = 147;
+
+a = 37; //give error like Assignment to constant variable
+
+console.log(a);
+
+// inncorrect method 
+
+const b;
+b = 55;
+console.log(b);
