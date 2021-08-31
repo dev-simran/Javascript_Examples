7df2fc746831a877fb284f0defea6c7019a8bf5a
simran-ux <simranjeet2772000@gmail.com>
2021-08-31 09:51:32 +0530

    DataTypes in JS

---
+++ DataTypes.js
@@ -0,0 +1,8 @@
+// Data type defines the values that a variable can take
+// Primitive data types
+
+var myVaribale = 1; //numeric Value
+myVaribale = 1.1  //decimal value
+myVaribale = 'Hello' //string value
+myVaribale = true; //Boolean value
+myVaribale = null; //null value
