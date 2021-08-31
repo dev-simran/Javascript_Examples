bee5ad3005c542035f7dbb0f2c9b82f047f82e61
simran-ux <simranjeet2772000@gmail.com>
2021-08-31 23:16:07 +0530

    Strings in JS

---
+++ string2.js
@@ -0,0 +1,10 @@
+// Since, String is character index, it can be accessed using for loop and for-of loop
+
+var str = 'HELLO';
+
+
+//loop 
+
+for( var i = 0; i < str.length; i++);
+
+document.write(str[1]);
