3fd190c95bf76713a49fdb39a8d5a0a52ffb6cd3
simran-ux <simranjeet2772000@gmail.com>
2021-08-31 10:10:28 +0530

    Strings in JS

---
+++ String.js
@@ -0,0 +1,8 @@
+// String is prmitive data type in javascript. A string is a textual content. 
+// it must be enclosed in single or double quotation marks.
+
+var str1 = 'Hello World'; //nsingle and double both quotation marks are acceptable in js 
+var str2 = "Bonsior Elliot"; //assigned string using assignment operator
+
+console.log("The first String is = ",str1);
+console.log("The Second String is = ",str2);
