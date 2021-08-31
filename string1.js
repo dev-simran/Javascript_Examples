74bcc9f9cf36b8b378c0c621089ffbce4d1df346
simran-ux <simranjeet2772000@gmail.com>
2021-08-31 11:02:56 +0530

    String length in JS

---
+++ string1.js
@@ -0,0 +1,17 @@
+//A String can also be treated like zero index based character array
+
+var str = 'HELLO';
+
+document.write(str[0]);
+document.write("<br>");
+document.write(str[1]);
+document.write("<br>");
+document.write(str[2]);
+document.write("<br>");
+document.write(str[3]);
+document.write("<br>");
+document.write(str[4]);
+document.write("<br>");
+
+
+document.write("The length of string is = ", str.length);
