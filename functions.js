74b89b253c494d5b9ddaf9b963444bf29531e233
simran-ux <simranjeet2772000@gmail.com>
2021-09-01 09:38:04 +0530

    Functions in JS

---
+++ functions.js
@@ -0,0 +1,11 @@
+// A Javascript function is a block of code designed to perform particular task
+// A JS function is executed when "Something" invokes it (calls it ).
+
+function multiply(a, b){
+
+    // Functionality
+
+    return a * b;
+}
+
+document.write(multiply(5,3));
