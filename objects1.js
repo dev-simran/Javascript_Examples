6074faf3d6afc1fa5b37d888339bc6c3a0b1e97e
simran-ux <simranjeet2772000@gmail.com>
2021-09-24 16:08:10 +0530

    Objects Methods in javascript

---
+++ objects1.js
@@ -0,0 +1,26 @@
+//Spaces and line Breaks are not important we can also declare object as
+
+const ravi = {
+
+    FirstName: 'Ravi',
+    LastName:'Kumar',
+    Age:'25'
+};
+
+document.write("The age of ravi is= ", ravi.Age);
+document.write(ravi);
+
+//JavaScript objects 
+//Javascript can also have methods 
+//Methods are stored in properties as functions
+
+const Rahul={
+
+    FirstName:'Rahul',
+    LastName:'Gupta',
+    Age:24,
+    FullName: function(){return this.FirstName + '' +this.LastName}
+
+};
+
+console.log("First Name of the user is = ", Rahul.FirstName);
