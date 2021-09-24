d40f87bf28b53de304fe989aa642acd2a88685a1
simran-ux <simranjeet2772000@gmail.com>
2021-09-24 10:33:41 +0530

    Objects in JavaScript

---
+++ objects.js
@@ -0,0 +1,13 @@
+// Objects are just like variables but it can holds multiple values
+//Declare objects with const keyword
+
+const Car = { Type:'Sport' , Model:2021 , Condition:'Excellent'};
+
+document.write("The type of car is: ", Car.Type);
+document.write("<br>");
+document.write("The Model of car is: ", Car.Model);
+document.write("<br>");
+document.write("The Condition of car is: ", Car.Condition);
+document.write("<br>");
+document.write("The Objec is: ", Car);
+
