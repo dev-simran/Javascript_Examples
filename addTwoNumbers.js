eb8b63a7b614b1c77549db5edae6011890e7b986
simran-ux <simranjeet2772000@gmail.com>
2021-10-08 22:12:23 +0530

    Program to add two Numbers

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -6,3 +6,13 @@ const num2 = 10;
 const sum = num1 + num2;
 
 console.log("The sum of " + num1 + " and " + num2 + " is : ", sum);
+
+
+//Add two numbers entered by the user
+
+const number1 = parseInt(prompt('Enter the first number:'));
+const number2 = parseInt(prompt('Enter the second number: '));
+
+const sum1 = number1 + number2 ;
+
+console.log(`The sum of ${number1} and ${number2} is: ${sum1}`);
