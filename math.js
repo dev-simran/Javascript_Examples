60c9a4d1764621eeca115173b14ebcdc48e93d03
simran-ux <simranjeet2772000@gmail.com>
2021-11-08 17:42:41 +0530

    Math object in JS

---
+++ math.js
@@ -0,0 +1,76 @@
+//js math objects allow us to perform mathematical operations on the numbers
+
+
+//Math.PI  
+
+document.write("The value of pi is = ", Math.PI);
+
+
+// Math object methods
+
+// number to integer 
+
+// Math.round()
+
+// it return the nearest integer 
+
+document.write("<br>");
+
+
+document.write("The nearest ineteger is = ", Math.round(4.7));
+
+// Math.ceil 
+// it returns the value of x rounded up to its nearest integer 
+// basically it returns the next largest integer 
+
+document.write("<br>");
+document.write("The value rounded up to its nearest integer is : ", Math.ceil(4.1));
+
+// Math.floor()
+
+// it returns the largest integer less than or equal to a given number 
+
+document.write("<br>");
+
+document.write("The vlaue rounded up to its larger less than or equal to nearest integer is : ", Math.floor(7.8));
+
+// Math.trunc()
+// it returns the inetger part of x 
+
+document.write("<br>");
+
+
+document.write("The inetger part of x uisng trunc() is = ", Math.trunc(4.7));
+
+
+// Math.sign() 
+// it returns the either a positive or negative +-1 
+document.write("<br>")
+document.write("The return value of sign() is = ", Math.sign(3));
+
+// Math.pow() 
+// it return the value of x to the power of y 
+
+document.write("<br>");
+document.write("The power of 2 is = ", Math.pow(2, 4))
+
+
+// Math.sqrt() 
+
+// it returns the square root of x 
+document.write("<br>")
+document.write("The square root of seven is = ", Math.sqrt(7));
+
+
+// Math.abs()
+// it return the positive value of x 
+
+document.write("<br>");
+document.write("The absolute vaue using abs() is = ", Math.abs(3-5));
+
+document.write("<br>");
+
+
+// Math.random()
+
+document.write("The random number is = ", Math.random());
